33612cf1fdd552b95f9becca1f08102d078016d8
Ken Roberts <kr26633z@gmail.com>
2017-02-06 21:08:25 +0000

    working on printing files in directories and thier sizes in the console

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ found_dirs = get_dir(path_selected)
 i = len(found_dirs)
 
 for x in range(i):
+    #testing the data structure
     file_name = found_dirs[x][0]['fName']
     file_size = found_dirs[x][0]['fSize']
     print(file_name + " -- " + str(file_size))
