0b2b868e46c04edfea403f9ac77429242f6bd4b6
Ken Roberts <kr26633z@gmail.com>
2017-02-06 01:32:20 +0000

    directory file structure being pulled correctly

--- Files.py
+++ Files.py
@@ -3,26 +3,32 @@ import os
 
 # inspect directory, for each file: strip out temp files and get size
 def get_dir(path):
+
+    found_dirs = []
+    for (paths, dirs, files) in os.walk(path):
+        for dir in dirs:
+            found_dirs.append(dir)
+
+    drive = []
     directory_files = []
 
-    dir = os.listdir(path)
+    for i in found_dirs:
+        dir = os.listdir(path+'/'+i)
+        for file in dir:
+            if not file.startswith('.') and not file.startswith('~'):
+                # appends dir path to filename
+                file_size = get_file_stats(path + '/' + i + '/' + file)
 
-    for file in dir:
+                # build dict for file and add to list
+                file_data = {'fName': file, 'fSize': file_size}
+                directory_files.append(file_data)
+                directory_files_sorted_keys = sorted(directory_files, key=lambda k: k['fSize'], reverse = True)
+                directory_files = directory_files_sorted_keys
+        drive.append(directory_files)
+        directory_files = []
+    return drive
 
-        if not file.startswith('.') and not file.startswith('~'):
-            # appends dir path to filename
-            file_size = get_file_stats(path+'/'+file)
 
-            # build dict for file and add to list
-            file_data = {'fName': file, 'fSize': file_size, 'dir': dir}
-            directory_files.append(file_data)
-
-        directory_files_sorted_keys = sorted(directory_files, key=lambda k: k['fSize'], reverse = True)
-        directory_files = directory_files_sorted_keys
-
-    return directory_files
-
-# show results
 def get_file_stats(file):
-     size = os.path.getsize(file)
-     return size
+    size = os.path.getsize(file)
+    return size
--- Main.py
+++ Main.py
@@ -1,15 +1,7 @@
 from Files import *
 
 path_selected = '/Users/kenroberts/Downloads'
-directory = os.listdir(path_selected)
-files = get_dir(path_selected)
+found_dirs = get_dir(path_selected)
 
-# for path in paths:
-
-#
-#     for dirs in directorys:
-#          print(dirs)
-
-#
-for file in files:
-    print(str(file['fName']) + " " + str(file['fSize']))
+for dirs in found_dirs:
+    print(dirs)
