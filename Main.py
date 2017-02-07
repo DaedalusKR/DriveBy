79bfb24d4040f5b8621a79b290f8c5e701388b1a
Ken Roberts <kr26633z@gmail.com>
2017-02-07 20:03:52 +0000

    print cycle working

--- Main.py
+++ Main.py
@@ -3,11 +3,18 @@ from Files import *
 path_selected = '/Users/kenroberts/Downloads'
 found_dirs = get_dir(path_selected)
 
-i = len(found_dirs)
 
-for x in range(i):
+num_dirs = len(found_dirs)
+
+for dir_count in range(num_dirs):
     #testing the data structure
-    file_name = found_dirs[x][0]['fName']
-    file_size = found_dirs[x][0]['fSize']
-    print(file_name + " -- " + str(file_size))
+    num_files = len(found_dirs[dir_count])
+    print('')
+    print('----------------' + str(found_dirs[dir_count][0]['fDir']) + '------------')
+    print('')
+    for file_count in range(num_files):
+
+        file_name = found_dirs[dir_count][file_count]['fName']
+        file_size = found_dirs[dir_count][file_count]['fSize']
+        print(file_name + " -- " + str(file_size))
 
