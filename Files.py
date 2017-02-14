1d099a21e7088118824a30d63673bd08e2812023
Ken Roberts <kr26633z@gmail.com>
2017-02-14 23:49:09 +0000

    trying to fix dir hier issues

--- Files.py
+++ Files.py
@@ -3,33 +3,32 @@ import os
 
 # inspect directory, for each file: strip out temp files and get size
 def get_dir(path):
-
-    found_dirs = []
-    for (paths, dirs, files) in os.walk(path):
-        for dir in dirs:
-            found_dirs.append(dir)
-
-    found_dirs.append('./')
-    drive = []
     directory_files = []
+    drive = []
+    file_paths = []
+    list = []
 
-    for i in found_dirs:
 
-        dir = os.listdir(path+'/'+i)
+    for (paths, dirs, files) in os.walk(path):
 
-        for file in dir:
-            if not file.startswith('.') and not file.startswith('~') and not os.path.isdir(path + '/' + i + '/' + file):
-                # appends dir path to filename
-                file_size = get_file_stats(path + '/' + i + '/' + file)
 
-                # build dict for file and add to list
-                file_data = {'fName': file, 'fSize': file_size, 'fDir': path + '/' + i}
-                directory_files.append(file_data)
-                directory_files_sorted_keys = sorted(directory_files, key=lambda k: k['fSize'], reverse = True)
-                directory_files = directory_files_sorted_keys
 
-        drive.append(directory_files)
-        directory_files = []
+        for name in files:
+            list.append(os.path.join(paths, name))
+
+            for file in list:
+                if not file.startswith('.') and not file.startswith('~') and not os.path.isdir(file):
+                    # appends dir path to filename
+                    file_size = get_file_stats(file)
+
+                    # build dict for file and add to list
+                    file_data = {'fName': file, 'fSize': file_size, 'fDir': path}
+                    directory_files.append(file_data)
+                    directory_files_sorted_keys = sorted(directory_files, key=lambda k: k['fSize'], reverse = True)
+                    directory_files = directory_files_sorted_keys
+
+            drive.append(directory_files)
+            directory_files = []
     return drive
 
 
