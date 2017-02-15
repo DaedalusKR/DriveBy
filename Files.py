62d92b9dcf72890fc6e95f65160489b33dca8685
Ken Roberts <kr26633z@gmail.com>
2017-02-15 21:55:18 +0000

    fixed file algorithm - need to check and work on multi-level

--- Files.py
+++ Files.py
@@ -3,36 +3,27 @@ import os
 
 # inspect directory, for each file: strip out temp files and get size
 def get_dir(path):
-    directory_files = []
+
+    found_dirs = []
     drive = []
-    file_paths = []
-    list = []
-
-
+    directory_files = []
     for (paths, dirs, files) in os.walk(path):
+        found_dirs.append(paths)
 
-        # for name in dirs:
-        #     print(os.path.join(paths, name))
-
-        for name in files:
-            list.append(os.path.join(paths, name))
-
-
-
-        for file in list:
-            if not file.startswith('.') and not file.startswith('~') and not os.path.isdir(file):
+    for name in found_dirs:
+        for file in os.listdir(os.path.join(paths, name)):
+            if not file.startswith('.') and not file.startswith('~'):
                 # appends dir path to filename
-                file_size = get_file_stats(file)
+                file_size = get_file_stats(os.path.join(paths, name, file))
 
                 # build dict for file and add to list
-                file_data = {'fName': file, 'fSize': file_size, 'fDir': path}
+                file_data = {'fName': file, 'fSize': file_size, 'fDir': os.path.join(paths, name)}
                 directory_files.append(file_data)
                 directory_files_sorted_keys = sorted(directory_files, key=lambda k: k['fSize'], reverse = True)
                 directory_files = directory_files_sorted_keys
 
         drive.append(directory_files)
         directory_files = []
-    print(drive)
     return drive
 
 
