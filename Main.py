c5bf289df69d191b19436e72fcea32eb39230501
Ken Roberts <kr26633z@gmail.com>
2017-02-19 12:36:24 +0000

    issue with alias resolved - os.path.islink([path])

--- Main.py
+++ Main.py
@@ -5,19 +5,7 @@ from PyQt5.QtGui import *
 from UI import *
 
 
-path_selected = '/Users/kenroberts'
-
-print(os.path.islink('/Users/kenroberts/Library/Application Support/Google/Chrome/RunningChromeVersion'))
-
-# #found_dirs = get_dir(path_selected)
-#
-# print(os.path.isfile('/Users/kenroberts/Library/Application Support/Google/Chrome/RunningChromeVersion'))
-#
-# file_test = open('/Users/kenroberts/Library/Application Support/Google/Chrome/RunningChromeVersion', 'r')
-# bytes = file_test.read(150)
-# print(bytes)
-
-
+path_selected = '/Users/kenroberts/Downloads'
 
 
 def main():
