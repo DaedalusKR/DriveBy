115d11728287d505a0cf9548158b8860a7b5fb82
Ken Roberts <kr26633z@gmail.com>
2017-02-23 23:13:35 +0000

    top directory out by few Mbs

--- Files.py
+++ Files.py
@@ -5,8 +5,11 @@ from PyQt5.QtWidgets import *
 # inspect directory, for each file: strip out temp files and get size
 def get_dir(path):
     tree_structure = QTreeWidgetItem()  # top tree struct init as selected path
+
+    top_path_size = 0
+    t_size = add_dirs_to_tree(path, tree_structure, top_path_size)
     tree_structure.setText(0, path)
-    add_dirs_to_tree(path, tree_structure)
+    tree_structure.setText(1, str(t_size))
     return tree_structure
 
 
@@ -15,26 +18,32 @@ def get_file_stats(file):
     return size
 
 
-def add_dirs_to_tree(path, tree_structure):
+def get_top_dir_size(path):
+    top_dir_contents = os.listdir(path)
+    t_dir_size = 0
+    for file in top_dir_contents:
+        t_dir_size += get_file_stats(os.path.join(path, file))
+    return t_dir_size
+
+
+def add_dirs_to_tree(path, tree_structure, top_path_size):
     dir_list = os.listdir(path)
+
+    top_path_size = 0
+
 
     for top_dir_file in dir_list:
 
         tree_structure_dir = QTreeWidgetItem()
-        dir_size = 0  # for dir tree line file size
 
         # file for top dir
         if not os.path.isdir(top_dir_file) and not top_dir_file.startswith('.') and not top_dir_file.startswith('~') and not os.path.islink(top_dir_file):
             tree_structure_dir.setText(0, top_dir_file)
             tree_structure_dir.setText(1, str(get_file_stats(os.path.join(path, top_dir_file))))
             tree_structure.addChild(tree_structure_dir)
-            top_dir_files = os.listdir(path)
-            for file in top_dir_files:
-                if not os.path.isdir(os.path.join(path, file)) and not file.startswith('.') and not file.startswith('~') and not os.path.islink(os.path.join(path, file)):
-                    file_size = get_file_stats(os.path.join(path, file))
+            top_path_size += get_file_stats(os.path.join(path, top_dir_file))
 
         dir_size = 0
-
         if os.path.isdir(os.path.join(path, top_dir_file)) and not top_dir_file.startswith('.') and not top_dir_file.startswith('~') and not os.path.islink(os.path.join(path, top_dir_file)):
 
             file_list = os.listdir(os.path.join(path, top_dir_file))
@@ -44,8 +53,10 @@ def add_dirs_to_tree(path, tree_structure):
                     # appends dir path to filename
                     file_size = get_file_stats(os.path.join(path, top_dir_file, file))
                     dir_size += file_size
+                    top_path_size += file_size
 
             tree_structure_dir.setText(1, str(dir_size))
-            add_dirs_to_tree(os.path.join(path, top_dir_file), tree_structure_dir)
-    return
+            add_dirs_to_tree(os.path.join(path, top_dir_file), tree_structure_dir, top_path_size)
+            print(top_path_size)
+    return top_path_size
 
